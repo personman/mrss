fcbb8486d255ce1eb2a6ac7aa8af607bf1f068d6
Danny Ferguson <personman2@gmail.com>
2013-03-05 12:15:10 -0600

    phpcs setup

--- module/Application/src/Application/Controller/IndexController.php
+++ module/Application/src/Application/Controller/IndexController.php
@@ -2,8 +2,8 @@
 /**
  * Zend Framework (http://framework.zend.com/)
  *
- * @link      http://github.com/zendframework/ZendSkeletonApplication for the canonical source repository
- * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc. (http://www.zend.com)
+ * @link      http://github.com/zendframework/ZendSkeletonApplication
+ * @copyright Copyright (c) 2005-2012 Zend Technologies USA Inc.
  * @license   http://framework.zend.com/license/new-bsd New BSD License
  */
 
