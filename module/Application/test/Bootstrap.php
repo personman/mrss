ca032189ea687fcdac00637bcaa98be5b4368540
Danny Ferguson <personman2@gmail.com>
2013-03-05 13:27:49 -0600

    enable codesniff warnings, fix warnings

--- module/Application/test/Bootstrap.php
+++ module/Application/test/Bootstrap.php
@@ -18,34 +18,36 @@ class Bootstrap
 
     public static function init()
     {
-        // Load the user-defined test configuration file, if it exists; otherwise, load
+        // Load the user-defined test configuration file, if it exists
         if (is_readable(__DIR__ . '/TestConfig.php')) {
             $testConfig = include __DIR__ . '/TestConfig.php';
         } else {
             $testConfig = include __DIR__ . '/TestConfig.php.dist';
         }
 
-        $zf2ModulePaths = array();
+        $zfModulePaths = array();
 
         if (isset($testConfig['module_listener_options']['module_paths'])) {
-            $modulePaths = $testConfig['module_listener_options']['module_paths'];
+            $modulePaths =
+                $testConfig['module_listener_options']['module_paths'];
             foreach ($modulePaths as $modulePath) {
                 if (($path = static::findParentPath($modulePath)) ) {
-                    $zf2ModulePaths[] = $path;
+                    $zfModulePaths[] = $path;
                 }
             }
         }
 
-        $zf2ModulePaths  = implode(PATH_SEPARATOR, $zf2ModulePaths) . PATH_SEPARATOR;
-        $zf2ModulePaths .= getenv('ZF2_MODULES_TEST_PATHS')
-            ?: (defined('ZF2_MODULES_TEST_PATHS') ? ZF2_MODULES_TEST_PATHS : '');
+        $zfModulePaths  =
+            implode(PATH_SEPARATOR, $zfModulePaths) . PATH_SEPARATOR;
+        $zfModulePaths .= getenv('ZF2_MODULES_TEST_PATHS') ?:
+            (defined('ZF2_MODULES_TEST_PATHS') ? ZF2_MODULES_TEST_PATHS : '');
 
         static::initAutoloader();
 
         // use ModuleManager to load this module and it's dependencies
         $baseConfig = array(
             'module_listener_options' => array(
-                'module_paths' => explode(PATH_SEPARATOR, $zf2ModulePaths),
+                'module_paths' => explode(PATH_SEPARATOR, $zfModulePaths),
             ),
         );
 
@@ -76,18 +78,19 @@ class Bootstrap
         if (is_readable($vendorPath . '/autoload.php')) {
             $loader = include $vendorPath . '/autoload.php';
         } else {
-            $zf2Path = getenv('ZF2_PATH')
-                ?: (defined('ZF2_PATH')
-                    ? ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ? $vendorPath . '/ZF2/library' : false));
+            $zfPath = getenv('ZF2_PATH') ?
+                (defined('ZF2_PATH') ?
+                    ZF2_PATH : (is_dir($vendorPath . '/ZF2/library') ?
+                        $vendorPath . '/ZF2/library' : false));
 
-            if (!$zf2Path) {
+            if (!$zfPath) {
                 throw new RuntimeException(
                     'Unable to load ZF2. Run `php composer.phar install`
                     or define a ZF2_PATH environment variable.'
                 );
             }
 
-            include $zf2Path . '/Zend/Loader/AutoloaderFactory.php';
+            include $zfPath . '/Zend/Loader/AutoloaderFactory.php';
 
         }
 
