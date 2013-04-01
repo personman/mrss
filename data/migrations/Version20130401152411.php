6d9868bf025633f88ec2b153f03497e72de832da
Danny Ferguson <dfergu15@jccc.edu>
2013-04-01 08:49:45 -0500

    doctrine migrations working, user module using doctrine, tests green

--- data/migrations/Version20130401152411.php
+++ data/migrations/Version20130401152411.php
@@ -8,23 +8,23 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20130327171916 extends AbstractMigration
+class Version20130401152411 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-        
-        $this->addSql("CREATE TABLE colleges (
-        id INT AUTO_INCREMENT NOT NULL,
-        name VARCHAR(255) NOT NULL,
-        ipeds VARCHAR(255) NOT NULL,
-        city VARCHAR(10) NOT NULL,
-        latitude DOUBLE PRECISION NOT NULL,
-        longitude DOUBLE PRECISION NOT NULL,
-        PRIMARY KEY(id))
-        DEFAULT CHARACTER SET utf8
-        COLLATE utf8_unicode_ci ENGINE = InnoDB");
+
+        $this->addSql("CREATE TABLE users (
+            id INT AUTO_INCREMENT NOT NULL,
+            username VARCHAR(255) DEFAULT NULL,
+            email VARCHAR(255) NOT NULL,
+            displayName VARCHAR(255) DEFAULT NULL,
+            password VARCHAR(255) NOT NULL,
+            state TINYINT(1) DEFAULT NULL,
+            PRIMARY KEY(id))
+            DEFAULT CHARACTER SET utf8
+            COLLATE utf8_unicode_ci ENGINE = InnoDB");
     }
 
     public function down(Schema $schema)
@@ -32,6 +32,6 @@ class Version20130327171916 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("DROP TABLE colleges");
+        $this->addSql("DROP TABLE users");
     }
 }
