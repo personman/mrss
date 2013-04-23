5b5edd73a71d25a3bbf6900370aff0c028ecfead
Danny Ferguson <dfergu15@jccc.edu>
2013-04-23 15:31:21 -0500

    fix behat sql file

--- features/bootstrap/behat-prepare-test-db.sql
+++ features/bootstrap/behat-prepare-test-db.sql
@@ -1,7 +1,11 @@
 -- This file prepares the test database for Behat scenarios. It is run before each one
 
 -- We start out by truncating some tables:
+ALTER TABLE pages DROP FOREIGN KEY FK_2074E575E37ECFB0;
 TRUNCATE users;
+ALTER TABLE pages ADD CONSTRAINT FK_2074E575E37ECFB0 FOREIGN KEY (updater_id)
+REFERENCES users (id);
+
 
 -- Now add some test data
 INSERT INTO users (email, displayName,password) VALUES ('dfergu15@jccc.edu', 'Dan McTest', '$2y$14$uCp4wgvaHPpvq/.Z3yvtzu7VLuKSphIROS8dLHEAduOo5LaZpvUnC');
