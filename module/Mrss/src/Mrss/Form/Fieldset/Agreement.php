280179f285b8caa48b5b260710071d93ddb3323c
Danny Ferguson <dfergu15@jccc.edu>
2013-05-08 12:29:39 -0500

    subscription form page 2: agreement

---
+++ module/Mrss/src/Mrss/Form/Fieldset/Agreement.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace Mrss\Form\Fieldset;
+
+use Zend\Form\Fieldset;
+use Zend\InputFilter\InputFilterProviderInterface;
+use Zend\Validator\Digits;
+
+class Agreement extends Fieldset implements InputFilterProviderInterface
+{
+    public function __construct()
+    {
+        parent::__construct('agreement');
+
+        $this->add(
+            array(
+                'name' => 'agree',
+                'type' => 'Checkbox',
+                'options' => array(
+                    'label' => 'Agreement',
+                    'help-block' => 'I agree to the terms above',
+                    'checked_value' => 1,
+                    'unchecked_value' => 'no'
+                ),
+                'attributes' => array(
+                    //'required' => true,
+                    'id' => 'agree',
+                    'description' => 'test'
+                )
+            )
+        );
+
+        $this->add(
+            array(
+                'name' => 'signature',
+                'type' => 'Text',
+                'options' => array(
+                    'label' => 'Electronic Signature'
+                ),
+                'attributes' => array(
+                    //'required' => true,
+                    'id' => 'signature'
+                )
+            )
+        );
+
+        $this->add(
+            array(
+                'name' => 'title',
+                'type' => 'Text',
+                'required' => true,
+                'options' => array(
+                    'label' => 'Title'
+                ),
+                'attributes' => array(
+                    //'required' => true,
+                    'id' => 'subscriber-title'
+                )
+            )
+        );
+
+        $this->add(
+            array(
+                'name' => 'authorization',
+                'type' => 'Checkbox',
+                'options' => array(
+                    'label' => 'Authorization',
+                    'help-block' => 'I hereby authorize my institution\'s
+                    participation in Maximizing Resources for Student Success',
+                    'checked_value' => 1,
+                    'unchecked_value' => 'no'
+                ),
+                'attributes' => array(
+                    //'required' => true,
+                    'id' => 'authorization'
+                )
+            )
+        );
+
+    }
+
+    /**
+     * Form validation
+     *
+     * @return array
+     */
+    public function getInputFilterSpecification()
+    {
+        return array(
+            'agree' => array(
+                'required' => true,
+                'validators' => array(
+                    array(
+                        'name' => 'Digits',
+                        'break_chain_on_failure' => true,
+                        'options' => array(
+                            'messages' => array(
+                                Digits::NOT_DIGITS => 'You must agree to the terms to
+                                subscribe'
+                            )
+                        )
+                    )
+                )
+            ),
+            'signature' => array(
+                'required' => true
+            ),
+            'title' => array(
+                'required' => true
+            ),
+            'authorization' => array(
+                'required' => true,
+                'validators' => array(
+                    array(
+                        'name' => 'Digits',
+                        'break_chain_on_failure' => true,
+                        'options' => array(
+                            'messages' => array(
+                                Digits::NOT_DIGITS => 'You must check the
+                                authorization checkbox to continue'
+                            )
+                        )
+                    )
+                )
+            )
+        );
+    }
+}
