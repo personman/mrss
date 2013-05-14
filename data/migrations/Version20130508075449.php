c06e12657629afde129b67280d039b6826d71eb6
Danny Ferguson <dfergu15@jccc.edu>
2013-05-14 10:17:08 -0500

    reorder migrations

--- data/migrations/Version20130508075449.php
+++ data/migrations/Version20130508075449.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration,
 /**
  * Auto-generated Migration: Please modify to your need!
  */
-class Version20130514075449 extends AbstractMigration
+class Version20130508075449 extends AbstractMigration
 {
     public function up(Schema $schema)
     {
