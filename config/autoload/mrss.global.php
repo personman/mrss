aaedd03da7553442f283633c1b048be80c0cb72d
Danny Ferguson <dfergu15@jccc.edu>
2013-06-06 15:48:34 -0500

    additional urls for studies

--- config/autoload/mrss.global.php
+++ config/autoload/mrss.global.php
@@ -3,6 +3,8 @@
 return array(
     'studies' => array(
         'mrss.dan.com' => 2,
-        'wtp.dan.com' => 3
+        'wtp.dan.com' => 3,
+        'beta.maximizingresources.org' => 2,
+        'maximizingresources.org' => 2
     )
 );
