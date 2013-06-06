48e8f1379193582355e184ba516e254bca07409b
Danny Ferguson <dfergu15@jccc.edu>
2013-06-06 09:37:05 -0500

    Create WTP study, multiple navigation

--- module/Mrss/src/Mrss/Service/AdminNavigationFactory.php
+++ module/Mrss/src/Mrss/Service/AdminNavigationFactory.php
@@ -3,11 +3,21 @@
 namespace Mrss\Service;
 
 use Zend\Navigation\Service\DefaultNavigationFactory;
+use Zend\ServiceManager\ServiceLocatorInterface;
 
 class AdminNavigationFactory extends DefaultNavigationFactory
 {
     protected function getName()
     {
         return 'admin';
+    }
+
+    public function getPages(ServiceLocatorInterface $serviceLocator)
+    {
+        $pages = parent::getPages($serviceLocator);
+
+        // change pages here
+
+        return $pages;
     }
 }
