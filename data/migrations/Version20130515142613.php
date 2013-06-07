fc758a65f76cff2128b1e54e1f0cbb0833796726
Danny Ferguson <dfergu15@jccc.edu>
2013-06-07 15:21:37 -0500

    reorder migrations

--- data/migrations/Version20130515142613.php
+++ data/migrations/Version20130515142613.php
@@ -15,6 +15,9 @@ class Version20130515142613 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
 
+
+        $this->addSql("ALTER TABLE studies ADD price DOUBLE PRECISION NOT NULL, ADD earlyPrice DOUBLE PRECISION NOT NULL, ADD earlyPriceDate DATE NOT NULL, ADD enrollmentOpen TINYINT(1) NOT NULL, ADD dataEntryOpen TINYINT(1) NOT NULL, ADD reportsOpen TINYINT(1) NOT NULL, ADD uPayUrl VARCHAR(255) NOT NULL, ADD uPaySiteId INT NOT NULL");
+
         // Create the NCCBP study. Use "ON DUPLICATE KEY" for when it's already
         // there. "id=id" means it just does nothing.
         $this->addSql("INSERT INTO studies (id, name, description, currentYear, uPaySiteId, uPayUrl) VALUES (1, 'NCCBP', 'National Community College Benchmark Project', 2013, 3, 'http://test.com') ON DUPLICATE KEY UPDATE id=id");
@@ -31,6 +34,8 @@ class Version20130515142613 extends AbstractMigration
 
         $this->addSql("DELETE FROM studies WHERE id = 1");
         $this->addSql("DELETE FROM studies WHERE id = 2");
+
+        $this->addSql("ALTER TABLE studies DROP price, DROP earlyPrice, DROP earlyPriceDate, DROP enrollmentOpen, DROP dataEntryOpen, DROP reportsOpen, DROP uPayUrl, DROP uPaySiteId");
 
     }
 }
--- data/migrations/Version20130606113300.php
+++ data/migrations/Version20130606113300.php
@@ -14,8 +14,7 @@ class Version20130606113300 extends AbstractMigration
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
-        
-        $this->addSql("ALTER TABLE studies ADD price DOUBLE PRECISION NOT NULL, ADD earlyPrice DOUBLE PRECISION NOT NULL, ADD earlyPriceDate DATE NOT NULL, ADD enrollmentOpen TINYINT(1) NOT NULL, ADD dataEntryOpen TINYINT(1) NOT NULL, ADD reportsOpen TINYINT(1) NOT NULL, ADD uPayUrl VARCHAR(255) NOT NULL, ADD uPaySiteId INT NOT NULL");
+
     }
 
     public function down(Schema $schema)
@@ -23,6 +22,6 @@ class Version20130606113300 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
         
-        $this->addSql("ALTER TABLE studies DROP price, DROP earlyPrice, DROP earlyPriceDate, DROP enrollmentOpen, DROP dataEntryOpen, DROP reportsOpen, DROP uPayUrl, DROP uPaySiteId");
+        
     }
 }
