99a31a37a53a15de5c7b1185bef2d5b979eac260
Danny Ferguson <dfergu15@jccc.edu>
2013-06-17 15:53:45 -0500

    navigation in footer, switch maximizingresources.org to wtp for review

--- config/autoload/mrss.global.php
+++ config/autoload/mrss.global.php
@@ -5,7 +5,7 @@ return array(
         'mrss.dan.com' => 3,
         'wtp.dan.com' => 3,
         'beta.maximizingresources.org' => 2,
-        'maximizingresources.org' => 2,
+        'maximizingresources.org' => 3,
         'localhost' => 2,
         'localhost:8182' => 2,
         'nhebi-build.jccc.edu' => 2
