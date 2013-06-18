8bbc1f706803b3829cb3ae306e84b2f6ccc1f110
Danny Ferguson <dfergu15@jccc.edu>
2013-06-18 14:31:41 -0500

    fix behat sql issue

--- features/bootstrap/behat-prepare-test-db.sql
+++ features/bootstrap/behat-prepare-test-db.sql
@@ -5,7 +5,8 @@ TRUNCATE users;
 
 
 -- Now add some test data
-INSERT INTO users (email, displayName, password, role) VALUES ('dfergu15@jccc.edu', 'Dan McTest', '$2y$14$uCp4wgvaHPpvq/.Z3yvtzu7VLuKSphIROS8dLHEAduOo5LaZpvUnC', 'admin');
+INSERT INTO users (email, displayName, password, role, college_id) VALUES ('dfergu15@jccc.edu', 'Dan McTest', '$2y$14$uCp4wgvaHPpvq/.Z3yvtzu7VLuKSphIROS8dLHEAduOo5LaZpvUnC',
+ 'admin', 1);
 
 
 -- Open up enrollment
