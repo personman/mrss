c60b9cf36413e24bd12375e45b23606b32bdfe18
Danny Ferguson <dfergu15@jccc.edu>
2013-06-21 17:33:27 -0500

    special styling for form 9

---
+++ public/js/data-entry.js
@@ -0,0 +1,7 @@
+$(function() {
+    // Some special formatting for form 9
+    $('#control-group-institutional_demographics_companies_less_than_50').before(
+        '<h3>Companies by employee size:</h3>' +
+            '<p>Companies by size (Use the US Economic Census, County Business Patterns to obtain these data)</p>'
+    )
+})
