f88cf49b1d56073bf460668c5cf9d675b97e1d70
Danny Ferguson <dfergu15@jccc.edu>
2013-06-21 09:59:46 -0500

    mysql index for users table

---
+++ data/migrations/Version20130621095742.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration,
+    Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your need!
+ */
+class Version20130621095742 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+
+        // Add some indexes to speed the site up
+        $this->addSql(
+            "CREATE INDEX users_email ON users(email)"
+        );
+        
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != "mysql", "Migration can only be executed safely on 'mysql'.");
+        
+    }
+}
