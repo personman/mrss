38209f16f584a25e97843af2ced3a5d9af328ad1
Danny Ferguson <dfergu15@jccc.edu>
2013-07-19 21:21:52 -0500

    system entity

--- module/Mrss/src/Mrss/Entity/System.php
+++ module/Mrss/src/Mrss/Entity/System.php
@@ -19,6 +19,17 @@ class System
     /** @ORM\Column(type="string") */
     protected $name;
 
+    /**
+     * @ORM\OneToMany(targetEntity="College", mappedBy="system")
+     * @ORM\OrderBy({"name" = "ASC"})
+     */
+    protected $colleges;
+
+    public function __construct()
+    {
+        $this->colleges = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
     public function setId($id)
     {
         $this->id = $id;
@@ -41,5 +52,17 @@ class System
     public function getName()
     {
         return $this->name;
+    }
+
+    public function setColleges($colleges)
+    {
+        $this->colleges = $colleges;
+
+        return $this;
+    }
+
+    public function getColleges()
+    {
+        return $this->colleges;
     }
 }
--- module/Mrss/test/MrssTest/Entity/SystemTest.php
+++ module/Mrss/test/MrssTest/Entity/SystemTest.php
@@ -24,6 +24,11 @@ class SystemTest extends PHPUnit_Framework_TestCase
     {
         $this->assertNull($this->system->getId());
         $this->assertNull($this->system->getName());
+
+        $this->assertInstanceOf(
+            '\Doctrine\Common\Collections\ArrayCollection',
+            $this->system->getColleges()
+        );
     }
 
     public function testSetters()
@@ -33,5 +38,21 @@ class SystemTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals(5, $this->system->getId());
         $this->assertEquals('test', $this->system->getName());
+    }
+
+    public function testAssociations()
+    {
+        $collegeMock = $this->getMock(
+            'Mrss\Entity\College'
+        );
+
+        $this->system->setColleges(array($collegeMock));
+        $colleges = $this->system->getColleges();
+
+        $this->assertTrue(is_array($colleges));
+        $this->assertInstanceOf(
+            'Mrss\Entity\College',
+            $colleges[0]
+        );
     }
 }
