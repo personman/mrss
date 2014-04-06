7909901bcf202681237f3605cf4e94b39f8402aa
Danny Ferguson <dfergu15@jccc.edu>
2014-04-06 13:16:44 -0500

    colorize percentiles on national report

---
+++ public/js/report.js
@@ -0,0 +1,22 @@
+$(function() {
+    // Colorize percentiles
+    $('.percentileColor').each(function() {
+        var td = $(this);
+        var value = parseFloat(td.html());
+
+        var breakPoints = [
+            {max: 25, color: '#B30000'},
+            {max: 75, color: '#000000'},
+            {max: 100, color: '#00B300'}
+        ];
+
+        for (var i in breakPoints) {
+            var point = breakPoints[i];
+
+            if (value <= point.max) {
+                td.css('color', point.color);
+                break;
+            }
+        }
+    })
+})
