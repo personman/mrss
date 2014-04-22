a96a7530adee7eddb87a8b7f555a43e8bcf8c9e9
Danny Ferguson <dfergu15@jccc.edu>
2014-04-22 08:02:55 -0500

    add unit test for benchmark importer

---
+++ module/Mrss/test/MrssTest/Service/ImportBenchmarksTest.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace MrssTest\Service;
+
+use PHPUnit_Framework_TestCase;
+use Mrss\Service\ImportBenchmarks;
+use Mrss\Entity\Benchmark;
+
+class ImportBenchmarksTest extends PHPUnit_Framework_TestCase
+{
+    /**
+     * @var ImportBenchmarks
+     */
+    protected $service;
+
+    public function setUp()
+    {
+        $this->service = new ImportBenchmarks();
+    }
+
+    public function tearDown()
+    {
+        unset($this->service);
+    }
+
+    public function testGetObservationPropertiesToAdd()
+    {
+        $benchmark = new Benchmark();
+        $benchmark->setDbColumn('does_not_exist');
+        $benchmark->setInputType('percent');
+
+        $this->service->checkObservation($benchmark);
+        $toAdd = $this->service->getObservationPropertiesToAdd();
+
+        $this->assertContains('@ORM', $toAdd);
+        $this->assertContains('does_not_exist', $toAdd);
+    }
+
+    public function testGetTypeByInputType()
+    {
+        $this->assertEquals('string', $this->service->getTypeByInputType('radio'));
+        $this->assertEquals('integer', $this->service->getTypeByInputType('number'));
+        $this->assertEquals('float', $this->service->getTypeByInputType('percent'));
+    }
+}
