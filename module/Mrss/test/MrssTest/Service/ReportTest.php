46f922fd8992fe204beef6f7784562d6923fa212
Danny Ferguson <dfergu15@jccc.edu>
2014-04-21 21:39:00 -0500

    unit tests

--- module/Mrss/test/MrssTest/Service/ReportTest.php
+++ module/Mrss/test/MrssTest/Service/ReportTest.php
@@ -34,4 +34,47 @@ class ReportTest extends PHPUnit_Framework_TestCase
 
         $this->assertEquals('report_calculated_3_2014', $key);
     }
+
+    /**
+     * @param $number
+     * @param $expected
+     * @dataProvider getOrdinalExamples
+     */
+    public function testGetOrdinal($number, $expected)
+    {
+        $this->assertEquals($expected, $this->report->getOrdinal($number));
+    }
+
+    public function getOrdinalExamples()
+    {
+        return array(
+            array(2, '2<sup>nd</sup>'),
+            array(3, '3<sup>rd</sup>'),
+            array(0.1, '<1<sup>st</sup>'),
+            array(99.99, '>99<sup>th</sup>'),
+            array(11, '11<sup>th</sup>')
+        );
+    }
+
+    public function testGetYourCollegeLabel()
+    {
+        $this->assertEquals('Your College', $this->report->getYourCollegeLabel());
+    }
+
+    public function testIsBenchmarkExclude()
+    {
+        $benchmarkMock = $this->getMock(
+            '\Mrss\Entity\Benchmark',
+            array('getDbColumn')
+        );
+        $benchmarkMock->expects($this->once())
+            ->method('getDbColumn')
+            ->will(
+                $this->returnValue('institutional_demographics_campus_environment')
+            );
+
+        $this->assertTrue(
+            $this->report->isBenchmarkExcludeFromReport($benchmarkMock)
+        );
+    }
 }
