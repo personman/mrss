cbd2616803ff956268b8977bc0854a20b50ba22b
Danny Ferguson <dfergu15@jccc.edu>
2014-10-10 15:35:04 -0500

    nccbp

--- module/Mrss/src/Mrss/Service/ImportNccbp.php
+++ module/Mrss/src/Mrss/Service/ImportNccbp.php
@@ -1143,7 +1143,7 @@ inner join content_field_data_entry_year y on y.nid = n.nid";
     public function getImports()
     {
         $imports = array(
-            'colleges' => array(
+            /*'colleges' => array(
                 'label' => 'Colleges',
                 'method' => 'importColleges'
             ),
@@ -1154,7 +1154,7 @@ inner join content_field_data_entry_year y on y.nid = n.nid";
             'users' => array(
                 'label' => 'Users',
                 'method' => 'importUsers'
-            ),
+            ),*/
             /*'benchmarkGroups' => array(
                 'label' => 'Benchmark Groups',
                 'method' => 'importBenchmarkGroups'
