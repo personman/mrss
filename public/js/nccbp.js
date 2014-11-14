f11003b069bb486ed2953438755020b66d887c01
Danny Ferguson <dfergu15@jccc.edu>
2014-11-14 13:46:49 -0600

    fancybox magnifying glass

--- public/js/nccbp.js
+++ public/js/nccbp.js
@@ -1,6 +1,9 @@
 $(function() {
     // Click login button to show form
     setupLoginButton()
+
+    // Fancybox image hovers
+    setupFancyboxHover()
 })
 
 function setupLoginButton()
@@ -11,4 +14,42 @@ function setupLoginButton()
 
         return false
     })
+}
+
+function setupFancyboxHover() {
+    $('.fancybox').each(function() {
+        var image = $(this).find('img')
+
+        // Add the overlay
+        var markup = '<span class="rollover"></span>'
+        $(this).prepend(markup)
+        var overlay = $(this).find(".rollover")
+
+
+        // OPACITY OF BUTTON SET TO 0% and width and height set
+        overlay
+            .css("opacity", "0")
+            .css('height', image.height())
+            .css('width', image.width())
+
+
+        // ON MOUSE OVER
+        overlay.hover(
+            function () {
+                // SET OPACITY TO 70%
+                $(this).stop().animate({
+                    opacity: .5
+                }, "fast");
+            },
+
+            // ON MOUSE OUT
+            function () {
+                // SET OPACITY BACK TO 0%
+                $(this).stop().animate({
+                    opacity: 0
+                }, "fast");
+            }
+        );
+    })
+
 }
