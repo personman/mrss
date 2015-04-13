4ea5a1eebc796bd738b3b746facc7d2bf2996bdb
Danny Ferguson <dfergu15@jccc.edu>
2015-04-13 12:55:27 -0500

    data entry debugging

--- module/Mrss/src/Mrss/Service/ComputedFields.php
+++ module/Mrss/src/Mrss/Service/ComputedFields.php
@@ -299,7 +299,7 @@ class ComputedFields
     public function calculateAllForObservation(Observation $observation)
     {
         if ($_SERVER['REMOTE_ADDR'] == '108.238.235.80') {
-            $this->debug = true;
+            //$this->debug = true;
         }
 
         if (empty($observation)) {
