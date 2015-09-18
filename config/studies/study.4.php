2dca2121fbb875cee7bd94e9d13810c87b3eb078
Danny Ferguson <dfergu15@jccc.edu>
2015-09-18 17:36:19 -0500

    update study 4 config

--- config/studies/study.4.php
+++ config/studies/study.4.php
@@ -6,7 +6,7 @@ return array(
     'layout' => 'nccbp.phtml',
     'favicon' => '/favicon.ico',
     'contact_instructions' => null,
-    'css' => 'nccbp.css'
+    'css' => 'nccbp.css',
     'css_print' => null
 );
 
