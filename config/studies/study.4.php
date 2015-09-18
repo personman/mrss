4246a1d5a5010cbe2ffccd52d7ed11f9d367e890
Danny Ferguson <dfergu15@jccc.edu>
2015-09-18 17:35:28 -0500

    update study 4 config

--- config/studies/study.4.php
+++ config/studies/study.4.php
@@ -3,4 +3,10 @@
 // AAUP - FCS
 return array(
     'breakpoints' => '20,40,60,80',
+    'layout' => 'nccbp.phtml',
+    'favicon' => '/favicon.ico',
+    'contact_instructions' => null,
+    'css' => null,
+    'css_print' => null
 );
+
