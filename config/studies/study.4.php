46ab50cdf26203ea87204b2eec84c2b30f731520
Danny Ferguson <dfergu15@jccc.edu>
2015-10-09 15:18:04 -0500

    move nhebi social links to config

--- config/studies/study.4.php
+++ config/studies/study.4.php
@@ -5,7 +5,7 @@ return array(
     'from_email' => 'jbarnshaw@aaup.org',
     //'cc_email' => 'jbarnshaw@aaup.org',
     'cc_email' => 'dfergu15@jccc.edu',
-    'approver_email' => 'jbarnshaw@aaup.org',
+    //'approver_email' => 'jbarnshaw@aaup.org',
     'approver_email' => 'dfergu15@jccc.edu',
     'breakpoints' => '20,40,60,80',
     'layout' => 'nccbp.phtml',
@@ -15,6 +15,7 @@ return array(
     'css_print' => null,
 	'header_title' => 'Faculty<br>Compensation<br>Survey',
     'user_role_choices' => 'viewer',
-    'copyright' => "1915 - " . date('Y') . " American Association of University Professors"
+    'copyright' => "1915 - " . date('Y') . " American Association of University Professors",
+    'footerLogo' => ''
 );
 
--- config/studies/study.default.php
+++ config/studies/study.default.php
@@ -13,5 +13,12 @@ return array(
     'css_print' => null,
     'header_title' => null,
     'user_role_choices' => 'viewer,contact,data', // Comma separated, no spaces
-    'copyright' => "2004 - " . date('Y') . " Johnson County Community College"
+    'copyright' => "2004 - " . date('Y') . " Johnson County Community College",
+    'footerLogo' => '<a href="/benchmarking-institute"><img src="/images/benchmark_logo.png" /></a>',
+    'footerSocial' => '<a href="http://twitter.com/EdBenchmark" title="Follow us on Twitter" target="_blank">
+                    <img src="/images/48x48_twitter.png" alt="Follow us on Twitter" />
+                </a>
+                <a href="https://www.linkedin.com/groups/National-Higher-Education-Benchmarking-Institute-2022361" title="Join our LinkedIn Group" target="_blank">
+                    <img src="/images/48x48_linkedin.png" alt="Join our LinkedIn Group" />
+                </a>'
 );
