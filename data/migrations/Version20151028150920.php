e871cefeade788fad3b0a3c5a2a320fc58e9ab9c
Danny Ferguson <dfergu15@jccc.edu>
2015-10-28 15:17:59 -0500

    more migrations (disabled) for fcs

---
+++ data/migrations/Version20151028150920.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20151028150920 extends AbstractMigration
+{
+    /**
+     * @param Schema $schema
+     */
+    public function up(Schema $schema)
+    {
+        return false;
+
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE observations CHANGE institution_conversion_factor institution_conversion_factor DOUBLE PRECISION DEFAULT NULL');
+    }
+
+    /**
+     * @param Schema $schema
+     */
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
+
+        $this->addSql('ALTER TABLE observations CHANGE institution_conversion_factor institution_conversion_factor INT DEFAULT NULL');
+    }
+}
