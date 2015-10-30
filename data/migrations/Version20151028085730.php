94f09fc158de3cbabc10943051b1ec11a425a4fb
Danny Ferguson <dfergu15@jccc.edu>
2015-10-30 09:19:35 -0500

    disable one migration

--- data/migrations/Version20151028085730.php
+++ data/migrations/Version20151028085730.php
@@ -15,6 +15,9 @@ class Version20151028085730 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
+
+        return false;
+        
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
