b040eba2ea51a97c53066531fb28d4680f88350e
Danny Ferguson <dfergu15@jccc.edu>
2015-12-09 15:45:12 -0600

    handling repeat issues

--- module/Mrss/src/Mrss/Service/Validation.php
+++ module/Mrss/src/Mrss/Service/Validation.php
@@ -17,49 +17,90 @@ class Validation
 
     protected $changeSet;
 
+    protected $existingIssues = array();
+
+    protected $issues = array();
+
     public function validate($observation, $priorObservation = null)
     {
-        $this->clearExistingIssues();
+        $this->collectExistingIssues();
 
-        $issues = array();
         $validator = $this->getValidator();
-
         if (!empty($validator)) {
             $issues = $validator->runValidation($observation, $priorObservation);
             $this->saveIssues($issues, $observation);
         }
 
+        $this->clearResolvedIssues();
+
+        $issues = $this->getIssues();
+
         return $issues;
     }
 
-    protected function clearExistingIssues()
+    protected function collectExistingIssues()
     {
-        $this->getIssueModel()->deleteByCollege($this->getUser()->getCollege());
+        // We want to key these with the error code
+        $issues = $this->getIssueModel()->findByCollege($this->getUser()->getCollege());
+        $keyedIssues = array();
+
+        foreach ($issues as $issue) {
+            $keyedIssues[$issue->getErrorCode()] = $issue;
+        }
+
+        $this->existingIssues = $keyedIssues;
     }
 
     public function saveIssues($issues, $observation)
     {
         foreach ($issues as $issueInfo) {
-            $issue = new Issue;
+            $errorCode = $issueInfo['errorCode'];
+
+            // Do we already have a row for this issue?
+            if (!empty($this->existingIssues[$errorCode])) {
+                $issue = $this->existingIssues[$errorCode];
+                unset($this->existingIssues[$errorCode]);
+            } else {
+                $issue = new Issue;
+
+                $issue->setStudy($this->getStudy());
+                $issue->setYear($observation->getYear());
+                $issue->setCollege($observation->getCollege());
+                $issue->setErrorCode($errorCode);
+                $issue->setFormUrl($issueInfo['formUrl']);
+            }
+
             $issue->setMessage($issueInfo['message']);
-            $issue->setStudy($this->getStudy());
-            $issue->setYear($observation->getYear());
-            $issue->setCollege($observation->getCollege());
-            $issue->setErrorCode($issueInfo['errorCode']);
-            $issue->setFormUrl($issueInfo['formUrl']);
             $issue->setUser($this->getUser());
 
             if ($changeSet = $this->getChangeSet()) {
                 $issue->setChangeSet($changeSet);
             }
 
-
-            // @todo: handle existing
+            // Hold on to non-resolved issues to alert the user of them
+            if (empty($issue->getStatus())) {
+                $this->issues[] = $issue;
+            }
 
             $this->getIssueModel()->save($issue);
         }
 
         $this->getIssueModel()->getEntityManager()->flush();
+    }
+
+    protected function clearResolvedIssues()
+    {
+        // Any issues left in this list now can be dropped
+        foreach ($this->existingIssues as $issue) {
+            $this->getIssueModel()->delete($issue);
+        }
+
+        $this->getIssueModel()->getEntityManager()->flush();
+    }
+
+    protected function getIssues()
+    {
+        return $this->issues;
     }
 
     public function setStudy($study)
