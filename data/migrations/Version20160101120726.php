0b4068f0fab9c1dec16919d3e66ff1a82bd437ea
Danny Ferguson <dfergu15@jccc.edu>
2016-01-01 12:09:07 -0600

    comment out fcs-only db migration

--- data/migrations/Version20160101120726.php
+++ data/migrations/Version20160101120726.php
@@ -18,7 +18,7 @@ class Version20160101120726 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE observations CHANGE institution_sector institution_sector VARCHAR(255) DEFAULT NULL, CHANGE carnegie_basic carnegie_basic VARCHAR(255) DEFAULT NULL, CHANGE institution_highest_degree institution_highest_degree VARCHAR(255) DEFAULT NULL, CHANGE institution_grants_medical_degree institution_grants_medical_degree VARCHAR(255) DEFAULT NULL, CHANGE institution_eligible_cip_codes institution_eligible_cip_codes VARCHAR(255) DEFAULT NULL');
+        //$this->addSql('ALTER TABLE observations CHANGE institution_sector institution_sector VARCHAR(255) DEFAULT NULL, CHANGE carnegie_basic carnegie_basic VARCHAR(255) DEFAULT NULL, CHANGE institution_highest_degree institution_highest_degree VARCHAR(255) DEFAULT NULL, CHANGE institution_grants_medical_degree institution_grants_medical_degree VARCHAR(255) DEFAULT NULL, CHANGE institution_eligible_cip_codes institution_eligible_cip_codes VARCHAR(255) DEFAULT NULL');
     }
 
     /**
@@ -29,6 +29,6 @@ class Version20160101120726 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE observations CHANGE institution_sector institution_sector INT DEFAULT NULL, CHANGE carnegie_basic carnegie_basic INT DEFAULT NULL, CHANGE institution_highest_degree institution_highest_degree INT DEFAULT NULL, CHANGE institution_grants_medical_degree institution_grants_medical_degree INT DEFAULT NULL, CHANGE institution_eligible_cip_codes institution_eligible_cip_codes INT DEFAULT NULL');
+        //$this->addSql('ALTER TABLE observations CHANGE institution_sector institution_sector INT DEFAULT NULL, CHANGE carnegie_basic carnegie_basic INT DEFAULT NULL, CHANGE institution_highest_degree institution_highest_degree INT DEFAULT NULL, CHANGE institution_grants_medical_degree institution_grants_medical_degree INT DEFAULT NULL, CHANGE institution_eligible_cip_codes institution_eligible_cip_codes INT DEFAULT NULL');
     }
 }
