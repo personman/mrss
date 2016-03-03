abb3f2e18b6cb0a0b6d71e2c12f1016535bc8988
Danny Ferguson <dfergu15@jccc.edu>
2016-03-03 13:27:01 -0600

    commenting out aaup-only migration

--- data/migrations/Version20160303132456.php
+++ data/migrations/Version20160303132456.php
@@ -18,7 +18,7 @@ class Version20160303132456 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() != 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE observations ADD ft_total_benefits_expenditure_professor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_associate_professor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_assistant_professor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_instructor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_lecturer_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_no_rank_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expentirue_no_diff_converted DOUBLE PRECISION DEFAULT NULL');
+        /*$this->addSql('ALTER TABLE observations ADD ft_total_benefits_expenditure_professor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_associate_professor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_assistant_professor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_instructor_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_lecturer_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expenditure_no_rank_converted DOUBLE PRECISION DEFAULT NULL, ADD ft_total_benefits_expentirue_no_diff_converted DOUBLE PRECISION DEFAULT NULL');*/
     }
 
     /**
