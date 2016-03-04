71b95d321719436d0a2aab0895e05174fc338955
Danny Ferguson <dfergu15@7611-C02R8482G8.attlocal.net>
2016-03-04 15:07:19 -0600

    show missing equations

--- module/Mrss/src/Mrss/Controller/AdminController.php
+++ module/Mrss/src/Mrss/Controller/AdminController.php
@@ -105,7 +105,10 @@ class AdminController extends AbstractActionController
             /** @var \Mrss\Entity\Benchmark $benchmark */
             if ($benchmark->getComputed()) {
                 $equation = $benchmark->getEquation();
-                if (!$this->getComputedFieldsService()->checkEquation($equation)) {
+
+                if (empty($equation)) {
+                    $results[] = $benchmark->getDbColumn() . " is computed but has no equation.";
+                } elseif (!$this->getComputedFieldsService()->checkEquation($equation)) {
                     $base = "Equation error for " . $benchmark->getDbColumn() . ". ";
                     $editLink = " <a href='/benchmark/study/0/edit/{$benchmark->getId()}'>Edit</a>";
                     $results[] = $base . $this->getComputedFieldsService()->getError() . $editLink;
