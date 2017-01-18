2a4dcc2f90fe0966cf2b4b2f64e21b6ec90705d3
Dan Ferguson <personman2@gmail.com>
2017-01-18 14:24:45 -0600

    enable benchmark heatmap for wf

--- config/studies/study.3.php
+++ config/studies/study.3.php
@@ -5,5 +5,6 @@
 return array(
     'favicon' => '/images/workforce-favicon.ico',
     'css' => 'workforce.css',
+    'benchmark_completion_heatmap' => false,
     'contact_instructions' => 'Contact us about benchmarks, data collection and reporting, subscription, peer comparison, or to simply comment on the Workforce&nbsp;Project. You can also reach us by phone: Dr. Lou Guthrie, Director, 913-469-8500 x4019 or Michelle Taylor, Senior Research &amp; Data Analyst, 913-469-8500 x3648.'
 );
