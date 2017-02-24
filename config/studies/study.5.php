fbedf5f6e856d722673467ab1b08cad45d422a03
Dan Ferguson <personman2@gmail.com>
2017-02-24 15:14:30 -0600

    envisio config

---
+++ config/studies/study.5.php
@@ -0,0 +1,11 @@
+<?php
+
+return array(
+    'layout' => 'nccbp.phtml',
+    'css' => 'nccbp.css',
+    'navigation' => 'nccbp_navigation',
+    'benchmark_completion_heatmap' => true,
+    'percent_change_report_columns' => array(
+    ),
+    'contact_instructions' => ''
+);
