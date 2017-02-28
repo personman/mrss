02fdfeeb40baa61f729b8dee75dee446fe8c57aa
Dan Ferguson <personman2@gmail.com>
2017-02-28 12:04:18 -0600

    email settings

--- config/studies/study.5.php
+++ config/studies/study.5.php
@@ -1,6 +1,12 @@
 <?php
 
 return array(
+    'from_email' => 'benchmarking@envisio.com',
+    'from_email_name' => 'Envisio Benchmarking',
+    'reply_to_email' => 'mbell@envisio.com',
+    'reply_to_email_name' => 'Mike Bell',
+    'cc_email' => 'mbell@envisio.com',
+    'approver_email' => 'mbell@envisio.com',
     'layout' => 'nccbp.phtml',
     'css' => 'nccbp.css',
     'navigation' => 'nccbp_navigation',
