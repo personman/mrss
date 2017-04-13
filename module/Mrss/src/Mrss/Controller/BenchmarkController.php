0032d117144956dcf2aa587c0ae7faf14d26de29
Dan Ferguson <personman2@gmail.com>
2017-04-13 18:05:13 -0500

    default years

--- module/Mrss/src/Mrss/Controller/BenchmarkController.php
+++ module/Mrss/src/Mrss/Controller/BenchmarkController.php
@@ -152,6 +152,10 @@ class BenchmarkController extends AbstractActionController
                 );
             }
 
+        } else {
+            // Set default years
+            $form->get('yearsAvailable')
+                ->setValue(array(2009,2010,2011,2012,2013,2014,2015,2016,2017,2018,2019,2020));
         }
 
         return array(
