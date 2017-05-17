e0e210b0ea4c910997ff72e2cba3d42bd6b2d302
Dan Ferguson <personman2@gmail.com>
2017-05-17 09:26:22 -0500

    permissions for print

--- config/autoload/bjyauthorize.global.php
+++ config/autoload/bjyauthorize.global.php
@@ -210,7 +210,7 @@ return array(
                 // Data users:
                 array(
                     'controller' => 'observations',
-                    'action' => array('dataEntry', 'overview', 'import', 'export', 'submittedValues', 'template'),
+                    'action' => array('dataEntry', 'overview', 'import', 'export', 'submittedValues', 'template', 'print'),
                     'roles' => array('data')
                 ),
                 array(
