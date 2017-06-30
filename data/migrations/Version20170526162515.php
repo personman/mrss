a68d35e361068f82426d27a07fb751a8c607b810
Dan Ferguson <personman2@gmail.com>
2017-06-30 13:42:08 -0500

    migration workaround

--- data/migrations/Version20170526162515.php
+++ data/migrations/Version20170526162515.php
@@ -18,9 +18,9 @@ class Version20170526162515 extends AbstractMigration
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE outliers ADD system_id INT DEFAULT NULL');
-        $this->addSql('ALTER TABLE outliers ADD CONSTRAINT FK_C2FFCDD9D0952FA5 FOREIGN KEY (system_id) REFERENCES college_systems (id) ON DELETE CASCADE');
-        $this->addSql('CREATE INDEX IDX_C2FFCDD9D0952FA5 ON outliers (system_id)');
+        //$this->addSql('ALTER TABLE outliers ADD system_id INT DEFAULT NULL');
+        //$this->addSql('ALTER TABLE outliers ADD CONSTRAINT FK_C2FFCDD9D0952FA5 FOREIGN KEY (system_id) REFERENCES college_systems (id) ON DELETE CASCADE');
+        //$this->addSql('CREATE INDEX IDX_C2FFCDD9D0952FA5 ON outliers (system_id)');
         $this->addSql('ALTER TABLE reports ADD system_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE reports ADD CONSTRAINT FK_F11FA745D0952FA5 FOREIGN KEY (system_id) REFERENCES college_systems (id) ON DELETE CASCADE');
         $this->addSql('CREATE INDEX IDX_F11FA745D0952FA5 ON reports (system_id)');
