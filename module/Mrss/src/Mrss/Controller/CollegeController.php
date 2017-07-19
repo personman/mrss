38577773c55ccc8f06669976062033257664dee2
Dan Ferguson <personman2@gmail.com>
2017-07-19 09:27:43 -0500

    remove debug

--- module/Mrss/src/Mrss/Controller/CollegeController.php
+++ module/Mrss/src/Mrss/Controller/CollegeController.php
@@ -177,8 +177,6 @@ class CollegeController extends AbstractActionController
             }
 
 
-            pr($collegeData);
-
             $markers = json_encode($markers);
 
             $forCache = array(
