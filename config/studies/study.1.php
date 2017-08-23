5887d1edad60260701a02a256f471ba36a9edef2
Dan Ferguson <personman2@gmail.com>
2017-08-23 14:30:06 -0500

    nc from % change

--- config/studies/study.1.php
+++ config/studies/study.1.php
@@ -32,7 +32,7 @@ return array(
         //'total_cert_lic_certificates',
         //'credentials_percent_unduplicated_enrollment',
         //'institutional_demographics_ged_awarded',
-        'all_ce_courses_canceled'
+        //'all_ce_courses_canceled'
     ),
     'contact_instructions' => 'You can also reach us by phone: Dr. Lou Guthrie, Director, 913-469-8500 x4019 or Michelle Taylor, Senior Research &amp; Data Analyst, 913-469-8500 x3648.'
 );
