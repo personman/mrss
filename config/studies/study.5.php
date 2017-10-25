b9bf2a408def88214e38b5fc5b3016b03ef8d5ef
Dan Ferguson <personman2@gmail.com>
2017-10-25 15:37:38 -0500

    enable muut for envisio design app

--- config/studies/study.5.php
+++ config/studies/study.5.php
@@ -44,7 +44,7 @@ return array(
     'footerLogo' => '',
     'footerSocial' => '',
     'freemium' => true,
-    'muut-disable' => array(
+    'muut' => array(
         'name' => 'govbenchmark',
         'key' => 'EbrH8g4SRS',
         'secret' => '6ANKNxwOWChTfH62gsvbDa4Y'
